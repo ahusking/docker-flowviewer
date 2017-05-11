a8034bd3401d23f82fc943a4bd92e403cb5a975c
ahusking <andrew@husking.id.au>
2017-05-11 23:17:00 +1000

    Fixes for external FlowViewer

--- startcontainer.sh
+++ startcontainer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # usage:
 # ./startcontainer <containerID> <port number> <path to flow-capture.conf> <path to FlowViewer folder>
-docker run -d -n docker-flowviewer -v $3:/etc/flow-utils/flow-capture.conf -v $4:/usr/lib/cgi-bin/FlowViewer -p $2:80 $1 
+docker run -d -ti --name docker-flowviewer -v config/flow-capture.conf:/etc/flow-utils/flow-capture.conf -v config/FlowViewer_Configuration.pm:/usr/lib/cgi-bin/FlowViewer/FlowViewer_Configuration.pm -v $4:/usr/lib/cgi-bin/FlowViewer -p $2:80 $1  bash
