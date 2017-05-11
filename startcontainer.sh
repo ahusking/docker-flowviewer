d20147386b012673c39864c3835739db4406dbc8
Andrew Husking <andrew@husking.id.au>
2017-05-11 22:44:29 +1000

    Update startcontainer.sh

--- startcontainer.sh
+++ startcontainer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # usage:
-# ./startcontainer <containerID> <port number> <path to flow-capture.conf>
-docker run -d -n docker-flowviewer -v $3:/etc/flow-utils/flow-capture.conf -p $2:80 $1 
+# ./startcontainer <containerID> <port number> <path to flow-capture.conf> <path to FlowViewer folder>
+docker run -d -n docker-flowviewer -v $3:/etc/flow-utils/flow-capture.conf -v $4:/usr/lib/cgi-bin/FlowViewer -p $2:80 $1 
