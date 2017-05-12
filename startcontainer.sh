6d251ceadbfb74f45b2258173a4233b6bfe61065
ahusking <andrew@husking.id.au>
2017-05-13 00:14:08 +1000

    cleanup

--- startcontainer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# usage:
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# ./startcontainer <containerID> <port number> <path to flow-capture.conf> <path to FlowViewer folder>
-docker run -d -ti --name docker-flowviewer -v $DIR/data:/data/ -v $DIR/config/flow-capture.conf:/etc/flow-tools/flow-capture.conf -v $DIR/config/FlowViewer_Configuration.pm:/usr/lib/cgi-bin/FlowViewer/FlowViewer_Configuration.pm  -p $2:80 $1  bash
-bash scripts/CreateHostDirs.sh
