62ff7a3246b65066bc431926f6b78b04f55e1771
ahusking <andrew@husking.id.au>
2017-05-12 21:12:29 +1000

    startup process update

--- scripts/CreateHostDirs.sh
+++ scripts/CreateHostDirs.sh
@@ -3,8 +3,8 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 
     read HOSTDIR <<< $(echo $line | awk '{split($0,a," "); print a[8]}')
     if ! [ -z "${HOSTDIR}" ]; then
-	echo creating ..$HOSTDIR
-	mkdir -p ..$HOSTDIR
+	echo creating $HOSTDIR
+	mkdir -p $HOSTDIR
     fi
 done < "../config/flow-capture.conf"
 mkdir -p ../data/flows/all_routers
---
+++ scripts/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+service apache2 start
+service apache2 restart
+service FlowTracker start
+service FlowTracker restart
+
+
+command="/bin/bash /opt/scripts/CreateHostDirs.sh"
+job="0 * * * * $command"
+cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
